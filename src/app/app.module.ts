5189fb154fdd9643b3653167315e643976ec7953
Michele Bombardi <bmsoftware.co@gmail.com>
2018-07-05 13:46:32 +0200

    minor fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 
+import { registerLocaleData } from '@angular/common';
+import localeIt from '@angular/common/locales/it';
+registerLocaleData(localeIt);
 
 import { AppComponent } from './app.component';
 
 import { CalendarModule } from 'angular-calendar';
 import { SchedulerModule } from './modules/scheduler/scheduler.module';
-
-import { registerLocaleData } from '@angular/common';
-import localeIt from '@angular/common/locales/it';
-registerLocaleData(localeIt);
-
 
 @NgModule({
   declarations: [
